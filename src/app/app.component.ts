4be33488a45abc7bae6c9b99c206e168371e0792
Nathan Pickard <nathan.pickard87@gmail.com>
2019-06-25 21:53:58 -0700

    Adding Post model interface and private fetchposts method with pipe and map operators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
+import { Post } from './post.model';
 
 @Component({
   selector: 'app-root',
@@ -11,13 +14,13 @@ export class AppComponent implements OnInit {
 
   constructor(private http: HttpClient) { }
 
-  ngOnInit() { }
+  ngOnInit() {
+    this.fetchPosts();
+  }
 
-  onCreatePost(postData: { title: string; content: string }) {
+  onCreatePost(postData: Post) {
     // Send Http request
-    this.http.post('https://ang-http-bd40f.firebaseio.com/',
-      postData
-    )
+    this.http.post<{ name: string }>('https://ang-http-bd40f.firebaseio.com/post.json', postData)
       .subscribe(responseData => {
         console.log(responseData);
       });
@@ -25,9 +28,29 @@ export class AppComponent implements OnInit {
 
   onFetchPosts() {
     // Send Http request
+    this.fetchPosts();
   }
 
   onClearPosts() {
     // Send Http request
   }
+
+  private fetchPosts() {
+    this.http
+      .get<{ [key: string]: Post }>('https://ang-http-bd40f.firebaseio.com/post.json')
+      .pipe(
+        map(responseData => {
+          const postsArray: Post[] = [];
+          for (const key in responseData) {
+            if (responseData.hasOwnProperty(key)) {
+              postsArray.push({ ...responseData[key], id: key });
+            }
+          }
+          return postsArray;
+        }))
+      .subscribe(posts => {
+        console.log(posts);
+      });
+  }
 }
+
